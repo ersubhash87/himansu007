50b4537f26e1c6e98f3e66a1d7b16541fef8074f
ersubhash87 <ersubhash87@gmail.com>
2025-08-19 22:01:32 +0530

    first time

--- main.tf
+++ main.tf
@@ -1,1 +1,28 @@
-xyz
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "165cee21-e64a-41b1-bb9a-f8fbd82bf738"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  }
+}
